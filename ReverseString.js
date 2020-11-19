c2dcb2aef32db08d921aaebe8c7d52a37f2eb08d
Gwen Zhou <pri17mib@gmail.com>
2020-11-19 17:12:54 +0000

    Improve time complexity

--- ReverseString.js
+++ ReverseString.js
@@ -1,12 +1,15 @@
-function reverseString(temp){
-	var reverseStr = [];// use to store the character reversed 
-  	var len = temp.length;// the length of the target string
-  	for(i=0; i<=len+1; i++){
-  		reverseStr.push(temp[len-i-1]); //From last to first(0), the characters stored into the array
-  	}
-  	var resultStr = reverseStr.join("");// Needs to converse the array into a string with no seperator
-  	//console.log(resultStr); 
-  	return resultStr;
+//Version two, save half time. First vesrion can refer the commit history.
+function reverseString(input) {
+  var reverseStr =[]  // Use to store the string reversed
+  var leftpoint = 0; // two pointers, improve time complexity
+  var rightpoint = input.length - 1; // Left pointer point to the start of the original string, right one point to the end of the string
+  while (rightpoint >= leftpoint) {
+    reverseStr[leftpoint] = input[rightpoint];
+    reverseStr[rightpoint] = input[leftpoint];
+    rightpoint--;
+    leftpoint++;
+  }
+  return reverseStr.join('');// Convert the array into string without seperator
 }
-var targetStr = prompt('Please input a string to reverse:') // Allow user to input the string
-console.log('Reversed string:'+ reverseString(targetStr));// console print the reversed string
+var targetStr = prompt('Please input a string to reverse:'); // Allow user to input the string
+console.log('Reversed string:' + reverseString(targetStr)); // console print the reversed string
